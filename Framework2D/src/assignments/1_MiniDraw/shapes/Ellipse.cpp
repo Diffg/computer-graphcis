81195d0778a7aa7b43b2710f025ef0535cb5433c
Diffg <2316819251@qq.com>
2025-06-15 12:37:22 +0800

    add Ellipse.h and Ellipse.cpp

--- Framework2D/src/assignments/1_MiniDraw/shapes/Ellipse.cpp
+++ Framework2D/src/assignments/1_MiniDraw/shapes/Ellipse.cpp
@@ -1,15 +1,15 @@
-#include "rect.h"
+#include "Ellipse.h"
 
 #include <imgui.h>
-
+ 
 namespace USTC_CG
 {
-// Draw the rectangle using ImGui
-void Rect::draw(const Config& config) const
+// Draw the line using ImGui
+void Ellipse::draw(const Config& config) const
 {
     ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-    draw_list->AddRect(
+    draw_list->AddEllipse(
         ImVec2(
             config.bias[0] + start_point_x_, config.bias[1] + start_point_y_),
         ImVec2(config.bias[0] + end_point_x_, config.bias[1] + end_point_y_),
@@ -18,18 +18,14 @@ void Rect::draw(const Config& config) const
             config.line_color[1],
             config.line_color[2],
             config.line_color[3]),
-        0.f,  // No rounding of corners
-        ImDrawFlags_None,
+            0.f,
+            
         config.line_thickness);
 }
 
-void Rect::update(float x, float y)
+void Ellipse::update(float x, float y)
 {
     end_point_x_ = x;
     end_point_y_ = y;
 }
-
 }  // namespace USTC_CG
-
-//void AddEllipse(const ImVec2& center, const ImVec2& radius,
-// ImU32 col, float rot, int num_segments, float thickness);
